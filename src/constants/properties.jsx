e76722d625f2e17cd9df46097e926311ab979fb2
sushblock <sushilstudy2013@gmail.com>
2023-08-05 16:04:59 +0530

    jokes section added

--- src/constants/properties.jsx
+++ src/constants/properties.jsx
@@ -1,3 +1,3 @@
-const button_tags = {prev: "Prev", next: "Next", show: "Show Answer"};
+const button_tags = {prev: "Prev", next: "Next", show: "Show Answer", go_to_start: "Start"};
 
 export {button_tags};
--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -1,9 +1,147 @@
-import React from 'react'
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import { button_tags } from "../constants/properties";
+import { loadCategories } from "../utils/categoryUtils";
+import wockaJokes from "../assets/wocka.json";
+import stupidstuffJokes from "../assets/stupidstuff.json";
+import redditJokes from "../assets/reddit_jokes.json";
+import "../styles/Jokes.css"; // Import the CSS file for styling
 
-function Jokes() {
+const WockaJokes = lazy(() => import("../components/WockaJokes"));
+const StupidstuffJokes = lazy(() => import("../components/StupidstuffJokes"));
+const RedditJokes = lazy(() => import("../components/RedditJokes"));
+
+const Jokes = () => {
+  const [wockaJokeIndex, setWockaJokeIndex] = useState(0);
+  const [stupidstuffJokeIndex, setStupidstuffJokeIndex] = useState(0);
+  const [redditJokeIndex, setRedditJokeIndex] = useState(0);
+  const [currentCategory, setCurrentCategory] = useState("wocka");
+  const [categories, setCategories] = useState({
+    wocka: [],
+    stupidstuff: [],
+  });
+
+  useEffect(() => {
+    // Load categories from local storage or JSON files
+    const loadCategoriesData = async () => {
+      const loadedCategories = await loadCategories();
+      setCategories(loadedCategories);
+    };
+    loadCategoriesData();
+  }, []);
+
+  useEffect(() => {
+    // Set the current category and reset joke indexes when category changes
+    setCurrentCategory("wocka");
+    setWockaJokeIndex(0);
+    setStupidstuffJokeIndex(0);
+    setRedditJokeIndex(0);
+  }, [categories]);
+
+  const handleNextWockaJoke = () => {
+    if (wockaJokeIndex < wockaJokes.length - 1) {
+      setWockaJokeIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const handlePrevWockaJoke = () => {
+    if (wockaJokeIndex > 0) {
+      setWockaJokeIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleGoToStartWocka = () => {
+    setWockaJokeIndex(0);
+  };
+
+  const handleNextStupidstuffJoke = () => {
+    if (stupidstuffJokeIndex < stupidstuffJokes.length - 1) {
+      setStupidstuffJokeIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const handlePrevStupidstuffJoke = () => {
+    if (stupidstuffJokeIndex > 0) {
+      setStupidstuffJokeIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleGoToStartStupidstuff = () => {
+    setStupidstuffJokeIndex(0);
+  };
+
+  const handleNextRedditJoke = () => {
+    if (redditJokeIndex < redditJokes.length - 1) {
+      setRedditJokeIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const handlePrevRedditJoke = () => {
+    if (redditJokeIndex > 0) {
+      setRedditJokeIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleGoToStartReddit = () => {
+    setRedditJokeIndex(0);
+  };
+
   return (
-    <div>Jokes</div>
-  )
-}
+    <div className="jokes-container">
+      {/* Accordion for wocka.json */}
+      <div
+        className={`jokes-accordion ${currentCategory === "wocka" ? "active" : ""}`}
+        onClick={() => setCurrentCategory("wocka")}
+      >
+        <h3>Wocka Jokes</h3>
+        {currentCategory === "wocka" && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <WockaJokes
+              currentJokeIndex={wockaJokeIndex}
+              handlePrevJoke={handlePrevWockaJoke}
+              handleNextJoke={handleNextWockaJoke}
+              handleGoToStart={handleGoToStartWocka}
+            />
+          </Suspense>
+        )}
+      </div>
 
-export default Jokes
+      {/* Accordion for stupidstuff.json */}
+      <div
+        className={`jokes-accordion ${currentCategory === "stupidstuff" ? "active" : ""}`}
+        onClick={() => setCurrentCategory("stupidstuff")}
+      >
+        <h3>Stupidstuff Jokes</h3>
+        {currentCategory === "stupidstuff" && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <StupidstuffJokes
+              currentJokeIndex={stupidstuffJokeIndex}
+              handlePrevJoke={handlePrevStupidstuffJoke}
+              handleNextJoke={handleNextStupidstuffJoke}
+              handleGoToStart={handleGoToStartStupidstuff}
+            />
+          </Suspense>
+        )}
+      </div>
+
+      {/* Accordion for reddit_jokes.json */}
+      <div
+        className={`jokes-accordion ${currentCategory === "reddit" ? "active" : ""}`}
+        onClick={() => setCurrentCategory("reddit")}
+      >
+        <h3>Reddit Jokes</h3>
+        {currentCategory === "reddit" && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <RedditJokes
+              currentJokeIndex={redditJokeIndex}
+              handlePrevJoke={handlePrevRedditJoke}
+              handleNextJoke={handleNextRedditJoke}
+              handleGoToStart={handleGoToStartReddit}
+            />
+          </Suspense>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Jokes;
